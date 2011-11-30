dd147a9778bc6b2bf59ef8d6313ebdc18b886e38
suzan <suzan_aziz2000@yahoo.com>
2011-11-30 22:57:56 +0200

    save all deals in db

---
+++ db/1-suzanaddcountry.sql
@@ -0,0 +1,30 @@
+INSERT INTO `countries` (
+`Code` ,
+`Name`
+)
+VALUES (
+'EGY', 'Egypt'
+);
+
+
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('608', 'Cairo', 'EGY', '30.069688', '31.247063', 'EGP');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('609', 'Alexandria', 'EGY', '31.220435', '29.943123', 'EGP');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('ARE', 'United Arab Emirates');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('64', 'Dubai', 'ARE', '25.208669', '55.446396', 'AED');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('65', 'Abu Dhabi', 'ARE', '24.487149', '54.359894', 'AED');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('66', 'Sharja', 'ARE', '25.341544', '55.412064', 'AED');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('JOR', 'Jordan');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('1786', 'Amman', 'JOR', '31.951361', '35.932746', 'JOD ');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('LBN', 'Lebanon');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('2438', 'Beirut', 'LBN', '33.889227', '35.49561', 'USD ');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('SAU', 'Saudi Arabia');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('3174', 'Jeddah', 'SAU', '21.551453', '39.171524', 'SAR ');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('3173', 'Riyadh', 'SAU', '24.738101', '46.725998', 'SAR');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('3177', 'al-Dammam', 'SAU', '26.469651', '49.832411', 'SAR');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('4083', 'Al Khobar', 'SAU', '26.293954', '50.221238', 'SAR');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('KWT', 'Kuwait');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('2429', 'Kuwait', 'KWT', '29.372602', '47.455444', 'KWD');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('QAT', 'Qatar');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('2973', 'Doha', 'QAT', '25.352714', '51.494179', 'QAR');
+INSERT INTO `countries` (`Code`, `Name`) VALUES ('BHR', 'Bahrain');
+INSERT INTO `cities` (`ID`, `Name`, `CountryCode`, `lat`, `lon`, `currency`) VALUES ('149', 'Almanama', 'BHR', '26.217979', '50.583086', 'BHD');
--- src/java/deals/controller/DealController.java
+++ src/java/deals/controller/DealController.java
@@ -5,9 +5,11 @@
 package deals.controller;
 
 import deals.dao.DealDao;
+import deals.entity.Cities;
 import deals.entity.Deals;
 import deals.entity.Language;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -30,6 +32,9 @@ import org.htmlcleaner.TagTransformation;
 import org.htmlcleaner.XPatherException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -43,43 +48,198 @@ public class DealController extends MultiActionController {
         this.dealDao = dealDao;
     }
 
-    public ModelAndView dealHeader(HttpServletRequest request, HttpServletResponse response) throws IOException, XPatherException {
+    public ModelAndView dealHeader(HttpServletRequest request, HttpServletResponse response) throws IOException, XPatherException, ParseException {
         String FEBestDeal = request.getParameter("FEBestDeal");
         ModelAndView mv = new ModelAndView("dealHeader");
         setDeal(request, response);
-        Deals deal = new Deals();
-        deal.setBestDeal(1);
-        deal.setTitle("sssss");
-        Language en=(Language) dealDao.get(Language.class , 2);
-        deal.setLanguage(en);
-        deal.setCurrency("EGP");
-        deal.setBestDeal(0);
-        deal.setViews(0);
-        dealDao.save(deal);
         return mv;
     }
 
-    public void setDeal(HttpServletRequest request, HttpServletResponse response) throws IOException, XPatherException {
-        Deals deal = new Deals();
+    public void setDeal(HttpServletRequest request, HttpServletResponse response) throws IOException, XPatherException, ParseException {
+        String url = "http://dealoola.activedd.com/front/deals/searchdeals.htm?allCategories=1&allParents=0&langId=2&maxDiscount=100&maxPrice=2000&maxTime=maxValue&minDiscount=0&minPrice=0&partnersList=3&startNumber=0";
+        String categorylistIds = "";
         HtmlCleaner cleaner = new HtmlCleaner();
         CleanerProperties props = new CleanerProperties();
-        CleanerTransformations transformations = new CleanerTransformations();
-        TagNode tagNode = new HtmlCleaner(props).clean(new URL("http://dealoola.activedd.com/front/core/home.htm"));
+        Language en = (Language) dealDao.get(Language.class, 2);
+        /*
+         *  get all categories in dealoola search
+         */
+        TagNode categoriesNode = new HtmlCleaner(props).clean(new URL("http://dealoola.activedd.com/front/core/home.htm"));
+        Object[] categoriesDivObjs = categoriesNode.evaluateXPath("//div[@class='accordion_contant no_p']/div");
+        if (categoriesDivObjs != null && categoriesDivObjs.length >= 1) {
+            TagNode categoryNode = (TagNode) categoriesDivObjs[0];
+            List<TagNode> checkboxInputs = categoryNode.getElementListByName("input", true);
+            for (TagNode theInput : checkboxInputs) {
+                String theCategoryId = theInput.getAttributeByName("value");
+                categorylistIds = categorylistIds + "&categoriesList=" + theCategoryId;
+            }
+            url = url + categorylistIds;
 
-        Object[]  descListss = tagNode.evaluateXPath("//div[@class='accordion_contant no_p']/div");
-        TagNode categoryNode = (TagNode) descListss[0];
-        List<TagNode> checkboxInputs = categoryNode.getElementListByName("input", true);
-        for (TagNode theInput : checkboxInputs) {
-            String theCategoryId = theInput.getAttributeByName("value");
-            System.out.println("the category id = " +theCategoryId );
-        }
-        
-        
-       System.out.println("----------htmllle div contains input category  and ids-----"+cleaner.getInnerHtml(categoryNode));
+            /*
+             *  get all cities in our site and then get deal sfor each city
+             */
+
+            Iterator<Cities> cities = dealDao.findAll(Cities.class).iterator();
+
+            while (cities.hasNext()) {
+                Cities city = cities.next();
+                String cityid = "&cityId=" + city.getId();
+                String cityUrl = url + cityid;
+                /*
+                 *   when we get all deals from search page we will get deal link to get  deal details
+                 */
+                TagNode searchDealNode = new HtmlCleaner(props).clean(new URL(cityUrl));
+                Object[] searchDealsDiv = searchDealNode.evaluateXPath("//p[@class='f deal-breif']");
+                /*
+                 * searchDealsDiv.length  means iterat in all deals in search deal page  and get  deal link in dealoola 
+                 */
+                for (int dealNo = 0; dealNo < searchDealsDiv.length; dealNo++) {
+                    Deals deal = new Deals();
+                    BigDecimal dealPrice = BigDecimal.ZERO;
+                    BigDecimal dealVal = BigDecimal.ZERO;
+                    BigDecimal dealSave = BigDecimal.ZERO;
+                    BigDecimal dealdisc = BigDecimal.ZERO;
+                    TagNode dealNode = (TagNode) searchDealsDiv[dealNo];
+                    deal.setCurrency(city.getCurrency());
+                    List<TagNode> readLinkNode = dealNode.getElementListByName("a", true);
+                    if (categoriesDivObjs != null && categoriesDivObjs.length >= 1) {
+                        /*
+                         * deal link
+                         */
+                        String dealUrl = "http://dealoola.activedd.com" + readLinkNode.get(0).getAttributeByName("href");
+                        System.out.println("-------url" + dealUrl);
+                        TagNode dealPageNode = new HtmlCleaner(props).clean(new URL(dealUrl));
+ ///////// ////////////////////////////////////// deal title /////////////////////////////////////////
+                        Object[] dealTitleObjs = dealPageNode.evaluateXPath("//span[@class='deal-discount']/a");
+                        if (dealTitleObjs != null && dealTitleObjs.length >= 1) {
+                            TagNode titleNode = (TagNode) dealTitleObjs[0];
+                            String dealtitle = titleNode.getText().toString().trim();
+                            deal.setTitle(dealtitle);
+                        }
+
+  ////////////////////////////////////////// deal desc/////////////////////////////////////////
+
+                        Object[] dealDescObjs = dealPageNode.evaluateXPath("//p[@class='f deal-text']");
+
+                        if (dealDescObjs != null && dealDescObjs.length >= 1) {
+                            TagNode descNode = (TagNode) dealDescObjs[0];
+                            if (dealTitleObjs != null && dealTitleObjs.length >= 1) {
+                                TagNode titleNode = (TagNode) dealTitleObjs[0];
+                                titleNode.removeFromTree();
+                            }
+                            String dealDesc = descNode.getText().toString().trim();
+                            deal.setDescription(dealDesc);
+
+                        }
+  /////////////////////////////////////////////// deal image/////////////////////////////////////////
+
+                        Object[] dealIamgeObs = dealPageNode.evaluateXPath("//div[@class='deal_page_image f']");
+
+                        if (dealIamgeObs != null && dealIamgeObs.length >= 1) {
+                            TagNode dealIamgeNode = (TagNode) dealIamgeObs[0];
+                            List<TagNode> ImageLinkNode = dealIamgeNode.getElementListByName("img", true);
+                            String dealdImage = ImageLinkNode.get(0).getAttributeByName("src");
+                            deal.setPhoto(dealdImage);
+
+                        }
+  ////////////////////////////////////// deal value means first price before discount /////////////////////////////////////////
+
+                        Object[] dealvalueObjs = dealPageNode.evaluateXPath("//div[@class='f deal-price']");
+                        if (dealvalueObjs != null && dealvalueObjs.length >= 1) {
+                            TagNode dealvalues = (TagNode) dealvalueObjs[0];
+                            String dealvals = dealvalues.getText().toString().trim();
+                            Pattern p = Pattern.compile("-?\\d+");
+                            Matcher m = p.matcher(dealvals);
+                            if (m.find()) {
+                                dealVal = new BigDecimal(m.group());
+                                deal.setValue(dealVal);
+
+                            }
+
+                        }
+  ////////////////////////////////////// deal price means  price after discount /////////////////////////////////////////
+
+                        Object[] dealpriceObjs = dealPageNode.evaluateXPath("//a[@class='press-buy f-r']");
+                        if (dealpriceObjs != null && dealpriceObjs.length >= 1) {
+                            TagNode dealpriceNode = (TagNode) dealpriceObjs[0];
+                            String dealprice = dealpriceNode.getText().toString().trim();
+                            Pattern p = Pattern.compile("-?\\d+");
+                            Matcher m = p.matcher(dealprice);
+                            if (m.find()) {
+                                dealPrice = new BigDecimal(m.group());
+                                deal.setPrice(dealPrice);
+
+                            }
+                        }
+                        dealSave = dealVal.subtract(dealPrice);
+                        deal.setSaving(dealSave);
+  ////////////////////////////////////// deal  discount /////////////////////////////////////////
+
+                        Object[] dealDiscObs = dealPageNode.evaluateXPath("//div[@class='f-r saved-money']");
+                        if (dealDiscObs != null && dealDiscObs.length >= 1) {
+                            TagNode dealdiscNode = (TagNode) dealDiscObs[0];
+                            String dealdiscount = dealdiscNode.getText().toString().trim();
+                            Pattern p = Pattern.compile("-?\\d+");
+                            Matcher m = p.matcher(dealdiscount);
+                            if (m.find()) {
+                                dealdisc = new BigDecimal(m.group());
+                                deal.setDiscount(dealdisc);
+
+                            }
+                        }
+  ////////////////////////////////////// deal  end date when deal closed /////////////////////////////////////////
+
+                        Object[] dealDateObs = dealPageNode.evaluateXPath("//div[@class='deal_time_left']");
+                        if (dealDateObs != null && dealDateObs.length >= 1) {
+                            TagNode dealDateNode = (TagNode) dealDateObs[0];
+                            String dealdate = dealDateNode.getText().toString();
+                            Pattern p = Pattern.compile("-?\\d+");
+                            Matcher m = p.matcher(dealdate);
+                            int No = 1;
+                            Calendar currentDate = Calendar.getInstance();
+                            Date endDate = new Date();
+                            while (m.find()) {
+
+                                if (No == 1) {
+                                    int dealhoursNo = Integer.parseInt(m.group());
+                                    currentDate.add(Calendar.HOUR, dealhoursNo);
+                                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                    String dateNow = formatter.format(currentDate.getTime());
+
+                                    endDate = formatter.parse(dateNow);
+                                }
+                                if (No == 2) {
+                                    int dealMinuteNo = Integer.parseInt(m.group());
+                                    currentDate.add(Calendar.MINUTE, dealMinuteNo);
+
+                                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                    String end = formatter.format(currentDate.getTime());
+
+                                    endDate = formatter.parse(end);
+
+                                }
+                                No = No + 1;
+                            }
+
+                            deal.setEnd(endDate);
+
+                        }
+
+                    }
+
+                    deal.setCities(city);
+                    deal.setLanguage(en);
+                    deal.setViews(0);
+                    deal.setBestDeal(0);
+                    dealDao.save(deal);
+
+                }
+
+            }
 
         }
     }
-//      public void saveDeal(HttpServletRequest request, HttpServletResponse response){
-//      }
+}
+
 
 
